a21ecaaaf55986d4e4bd79ceb21bc75c5f02e5ae
Harshit <srivastava0798@gmail.com>
2026-02-05 20:42:34 +0530

    complete the hero-section , creating the home route

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,17 +1,25 @@
 import './App.css'
+import Hero from './component/Hero';
 import Navbar from './component/Navbar'
-import { useLocation } from 'react-router-dom'
+import { Routes, useLocation ,Route} from 'react-router-dom'
 
 
 
 function App() {
- 
-   const isOwnerPath = useLocation().pathname.includes('owner') ;
+
+  const isOwnerPath = useLocation().pathname.includes('owner');
 
   return (
     <div>
-      {!isOwnerPath &&  <Navbar/> }
-    
+      {!isOwnerPath && <Navbar />}
+
+      <div className='min-h-[70vh]'>
+        <Routes>
+          <Route path='/' element={<Hero />} />
+
+        </Routes>
+      </div>
+
     </div>
   )
 }
