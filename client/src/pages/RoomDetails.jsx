cff46768601ec11fd51f2e292aa64a2354b6b8cd
Harshit <srivastava0798@gmail.com>
2026-02-08 14:56:08 +0530

    compelete the highlight section in room detail page

--- client/src/pages/RoomDetails.jsx
+++ client/src/pages/RoomDetails.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import { assets, roomsDummyData } from "../assets/assets";
+import { assets, facilityIcons, roomsDummyData } from "../assets/assets";
 import StarRating from "../component/StarRating";
 
 function RoomDetails() {
@@ -58,6 +58,27 @@ function RoomDetails() {
                 ))}
             </div>
         </div>
+
+       {/* Room Highlights */}
+       <div className="flex flex-col md:flex-row md:justify-between mt-10">
+            <div className="flex flex-col">
+                <h1>Experience Luxury Like Never Before</h1>
+            </div>
+            <div className="flex flex-wrap items-center mt-3 mb-6 gap-4">
+                {room.amenities.map((item,index)=>(
+                    <div key={index} className="flex items-center gap-2 px-3 py-2 rounded-lg bg-gray-100">
+                        <img src={facilityIcons[item] } alt={item} className="w-5 h-5"/>
+                        <p className="text-xs">{item}</p>
+
+                    </div>
+                ))}
+            </div>
+            {/* Room Price */}
+            <p className="text-2xl font-medium">${room.pricePerNight}/night</p>
+           
+       </div>
+         
+ 
     </div>
   );
 }
