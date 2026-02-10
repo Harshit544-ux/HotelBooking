1ea5f50666f31ccbeb2fe6ff2c2c4c7f6ce9a54e
Harshit <srivastava0798@gmail.com>
2026-02-10 21:48:25 +0530

    add the styling and user authentication in owner page

--- client/src/component/hotelOwner/Navbar.jsx
+++ client/src/component/hotelOwner/Navbar.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
+import { assets } from "../assets/assets";
+import { UserButton } from "@clerk/clerk-react";
 
-function Navbar() {
+const Navbar = () => {
   return (
-    <div>Navbar</div>
-  )
-}
+    <div
+      className="flex items-center justify-between 
+      px-4 md:px-8 py-3 
+      border-b border-gray-300 
+      bg-white transition-all duration-300"
+    >
+      {/* Logo */}
+      <Link to="/">
+        <img
+          src={assets.logo}
+          alt="logo"
+          className="h-9 invert opacity-80"
+        />
+      </Link>
 
-export default Navbar
+      {/* User */}
+      <UserButton />
+    </div>
+  );
+};
+
+export default Navbar;
