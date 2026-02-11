4a84e53653a0267afb305a5b113b317914a93053
Harshit <srivastava0798@gmail.com>
2026-02-11 20:22:19 +0530

    Add hotel owner layout with Navbar, Sidebar and nested routing support

--- client/src/pages/hotelOwner/Layout.jsx
+++ client/src/pages/hotelOwner/Layout.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
 import Navbar from '../../component/hotelOwner/Navbar'
+import Sidebar from '../../component/hotelOwner/Sidebar'
+import { Outlet } from 'react-router-dom'
 
 function Layout() {
   return (
     <div className='flex flex-col h-screen'>
         <Navbar/>
+        <div className='flex h-full'>
+          <Sidebar/>
+          <div className='flex-1 p-4 pt-10 md:px-10 h-full'>
+            <Outlet/>
+          </div>
+        </div>
     </div>
   )
 }
